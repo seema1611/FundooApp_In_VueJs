3a9f36249d119a921c4aa0efe09601b5c12be6b4
seema1611 <seemarajpure16@gmail.com>
2020-09-25 22:30:40 +0700

    [Seema]ADD: added the login page ui

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+export default {
+  name: 'App',
+  components:{
+    Home,
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  min-height: 750px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: white;
+  color: #2c3e50;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './Route'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+const router =new VueRouter({
+  routes:Routes
+})
+
+new Vue({
+  render: h => h(App),
+  router:router
+}).$mount('#app')
